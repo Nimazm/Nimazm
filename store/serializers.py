f3db83807d04e4f355444c572f7187592359c91e
Nimazm <nima.1381.zamani@gmail.com>
2023-03-10 22:44:19 +0330

    serializing relationship

---
+++ store/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Product,Collection
+from decimal import Decimal
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    unit_price = serializers.DecimalField(max_digits=6,decimal_places=2)
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_price')
+    collection = CollectionSerializer()
+    #collection = serializers.HyperlinkedRelatedField(
+     #   queryset=Collection.objects.all(),
+      #  view_name='collection-detail'
+    #)
+
+    def calculate_price(self,product:Product):
+        return product.unit_price*Decimal(1.2)
--- store/views.py
+++ store/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product,Collection
+from .serializers import ProductSerializer,CollectionSerializer
 # Create your views here.
 
+
+
+@api_view()
 def product_list(request):
-    return HttpResponse('ok')
+    #query_set = Product.objects.select_related('collection').all()
+    query_set = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(query_set,many=True,context={'request':request})
+    return Response(serializer.data)
 
+@api_view()
+def product_detail(request,id):
+    product = get_object_or_404(Product,pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
+@api_view
+def collection_detail(request,pk):
+    #collection = get_object_or_404(Collection, pk=pk)
+    #serializer = CollectionSerializer(collection)
+    #return Response(serializer.data)
+    return Response('ok')
